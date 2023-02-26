95e54f6f19cd6e162668d060a4b2775fd3885db8
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-02-27 00:22:43 +0600

    Project Setup & Navbar Done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,41 @@
-import '../styles/globals.css'
+import { ThemeProvider, useTheme } from "@material-tailwind/react";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const theme = {
+        button: {
+            styles: {
+                base: { initial: { textTransform: "", fontWeight: "" } },
+                sizes: { sm: { px: "px-5", fontSize: "text-base", borderRadius: "rounded-full" } },
+                variants: {
+                    filled: {
+                        blue: {
+                            active: "active:opacity-[0.85]",
+                            backgroud: "bg-gradient-to-tr from-primary via-secondary to-accent",
+                            color: "text-white",
+                            hover: "",
+                            shadow: "",
+                        },
+                    },
+                    outlined: {
+                        blue: {
+                            active: "active:opacity-[0.85]",
+                            border: "border border-primary",
+                            color: "text-primary",
+                            focus: "focus:bg-gradient-to-tr from-primary via-secondary to-accent focus:text-background focus:border-background",
+                            hover: "hover:bg-gradient-to-tr from-primary via-secondary to-accent hover:text-background hover:border-background",
+                        },
+                    },
+                },
+            },
+        },
+    };
+
+    console.log(useTheme());
+
+    return (
+        <ThemeProvider value={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
